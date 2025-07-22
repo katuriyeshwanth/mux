426e43ffe8d64e867df3bd6e5edb6b8bef4ffb98
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-22 22:29:06 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -27,3 +27,15 @@ mux4to1 m2(in[11:8],sel[1:0],t[2]);
 mux4to1 m3(in[15:12],sel[1:0],t[3]);
 mux4to1 m4(t,sel[3:2],out);
 endmodule
+
+Using if statement
+module mux21(in1,in0,s,f);
+input [3:0]in1,in0;
+input s;
+output reg [3:0]f;
+always @(*)
+if (s)
+f=in1;
+else
+f=in0;
+endmodule 
